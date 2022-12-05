46ffab46d350a9bdf8c6c33f397f375d208742ba
JackPadalino <pada0867@gmail.com>
2022-12-04 19:54:52 -0500

    updated item names in seed file

--- server/db/seed.js
+++ server/db/seed.js
@@ -14,7 +14,7 @@ const productsList = [
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   {
     name: "Wool-Blend Topcoat",
-    imageURL: "/img/jacket/jacket1.jpg",
+    imageURL: "/img/jacket/mensJacket1.jpg",
     description:
       "A topcoat is a staple in every sharp-dressed man's wardrobe. Pair this one over sweaters for effortless styling",
     color: "Blue",
@@ -25,7 +25,7 @@ const productsList = [
   },
   {
     name: "Puffer Bomber Jacket",
-    imageURL: "/img/jacket/jacket2.jpg",
+    imageURL: "/img/jacket/mensJacket2.jpg",
     description:
       "Warm-up your looks with this effortless puffer bomber jacket. Pair with jeans or chinos for the perfect everyday look",
     color: "Gray",
@@ -36,7 +36,7 @@ const productsList = [
   },
   {
     name: " Wool-Blend Hooded Topcoat",
-    imageURL: "/img/jacket/jacket3.jpg",
+    imageURL: "/img/jacket/mensJacket3.jpg",
     description:
       "With a twist on the classic, this wool-blend topcoat features a drawstring hood and a covered zip closure. Pair with jeans and boots for a casual look",
     color: "Brown",
@@ -47,7 +47,7 @@ const productsList = [
   },
   {
     name: "Wool-Blend Trucker Jacket",
-    imageURL: "/img/jacket/jacket4.jpg",
+    imageURL: "/img/jacket/mensJacket4.jpg",
     description:
       "The perfect classic trucker jacket in a navy wool-blend fabric. Add this to any outfit to keep cozy on cool days and chilly nights",
     color: "Blue",
@@ -58,7 +58,7 @@ const productsList = [
   },
   {
     name: "Wool-Blend Water Resistant Topcoat",
-    imageURL: "/img/jacket/jacket5.jpg",
+    imageURL: "/img/jacket/mensJacket5.jpg",
     description:
       "A topcoat is a staple in every sharp-dressed man's wardrobe. Pair this wool-blend coat with denim for a polished look or with suiting for all of your formal events this season",
     color: "Black",
@@ -69,7 +69,7 @@ const productsList = [
   },
   {
     name: "Cable Knit CrewNeck Sweater",
-    imageURL: "/img/sweater/sweater1.jpg",
+    imageURL: "/img/sweater/mensSweater1.jpg",
     description:
       "A winter classic, this cable knit crewneck sweater is the perfect cold weather essential. Complete the look with jeans or chinos for a sleek look",
     color: "White",
@@ -80,7 +80,7 @@ const productsList = [
   },
   {
     name: "Waffle Knit Shawl Collar Cardigan",
-    imageURL: "/img/sweater/sweater2.jpg",
+    imageURL: "/img/sweater/mensSweater2.jpg",
     description:
       "A classic layering choice to keep you warm this season. This waffle knit cardigan looks great over tees, button-ups and more - we love a versatile piece",
     color: "Black",
@@ -91,7 +91,7 @@ const productsList = [
   },
   {
     name: "Vine Cable Knit CrewNeck Sweater",
-    imageURL: "/img/sweater/sweater3.jpg",
+    imageURL: "/img/sweater/mensSweater3.jpg",
     description:
       "A winter classic with a unique cable knit pattern, this crewneck sweater brings a fun twist to a cold weather essential. Complete the look with jeans or chinos for a sleek look",
     color: "Blue",
@@ -102,7 +102,7 @@ const productsList = [
   },
   {
     name: "Solid Mock Neck Quarter Zip Sweater",
-    imageURL: "/img/sweater/sweater4.jpg",
+    imageURL: "/img/sweater/mensSweater4.jpg",
     description:
       "A cozy, ribbed sweater featuring a mock neck and quarter-zip front that's perfect for cool days and chilly nights. Pair with jeans, joggers or chinos for a polished casual look",
     color: "Brown",
@@ -113,7 +113,7 @@ const productsList = [
   },
   {
     name: "V-Neck Popover Sweater",
-    imageURL: "/img/sweater/sweater5.jpg",
+    imageURL: "/img/sweater/mensSweater5.jpg",
     description:
       "An everyday staple for cold weather months, this v-neck sweater is a versatile choice and pairs well any of your favorite bottoms",
     color: "Gray",
@@ -124,7 +124,7 @@ const productsList = [
   },
   {
     name: "Slim Light Wash Stretch Jeans",
-    imageURL: "/img/pants/pants1.jpg",
+    imageURL: "/img/pants/mensPants1.jpg",
     description:
       "This slim light wash jean features the perfect amount of stretch for all-day comfort. Throw on with sweaters or tees or a put together look",
     color: "Blue",
@@ -135,7 +135,7 @@ const productsList = [
   },
   {
     name: "Slim Dark Wash Selvedge Jeans",
-    imageURL: "/img/pants/pants2.jpg",
+    imageURL: "/img/pants/mensPants2.jpg",
     description:
       "Woven on a vintage shuttle loom which provides a highly desirable irregular yarn character indicative of prized premium denim. This guarantees that no two jeans are identical. Every jean is unique",
     color: "Dark Blue",
@@ -146,7 +146,7 @@ const productsList = [
   },
   {
     name: "Slim Hyper Stretch Jeans",
-    imageURL: "/img/pants/pants3.jpg",
+    imageURL: "/img/pants/mensPants3.jpg",
     description:
       "Our hyper stretch jeans are constructed with the maximum amount of stretch for all day comfort. Featuring a slim fit in a trendy color that provides a versatile look that pairs with any outfit",
     color: "Brown",
@@ -157,7 +157,7 @@ const productsList = [
   },
   {
     name: "Slim Straight Medium Wash Hyper Stretch Jeans",
-    imageURL: "/img/pants/pants4.jpg",
+    imageURL: "/img/pants/mensPants4.jpg",
     description:
       "These slim straight jeans feature our highest level of stretch for all-day comfort. Made in a versatile medium wash that's perfect for work or play",
     color: "Blue",
@@ -168,7 +168,7 @@ const productsList = [
   },
   {
     name: "Slim Straight Warm Stone Hyper Stretch Jeans",
-    imageURL: "/img/pants/pants5.jpg",
+    imageURL: "/img/pants/mensPants5.jpg",
     description:
       "This slim straight jean is made with our highest level of stretch to keep you comfortable no matter where the day takes you. Featured in a stylish warm stone wash to bring new life to your wardrobe",
     color: "Light Brown",
@@ -179,7 +179,7 @@ const productsList = [
   },
   {
     name: "Slim Geo Print Stretch Corduroy Shirt",
-    imageURL: "/img/shirt/shirt1,jpg",
+    imageURL: "/img/shirt/mensShirt1,jpg",
     description:
       "A soft, stretchy corduroy in a trendy geo print for the perfect blend of cozy and polished. Pair with chinos or jeans for effortless styling",
     color: "red",
@@ -190,7 +190,7 @@ const productsList = [
   },
   {
     name: "Floral Stretch Corduroy Shirt",
-    imageURL: "/img/shirts/shirt2.jpg",
+    imageURL: "/img/shirts/mensShirt2.jpg",
     description:
       "A soft, stretchy corduroy in a floral print for the perfect blend of cozy and polished. Pair with chinos or jeans for effortless styling as we head into fall",
     color: "Blue",
@@ -201,7 +201,7 @@ const productsList = [
   },
   {
     name: "Floral Stretch Corduroy Shirt",
-    imageURL: "/img/shirts/shirt3.jpg",
+    imageURL: "/img/shirts/mensShirt3.jpg",
     description:
       "A soft, stretchy corduroy in a floral print for the perfect blend of cozy and polished. Pair with chinos or jeans for effortless styling",
     color: "",
@@ -212,7 +212,7 @@ const productsList = [
   },
   {
     name: "Solid Corduroy Shirt",
-    imageURL: "/img/shirts/shirt4.jpg",
+    imageURL: "/img/shirts/mensShirt4.jpg",
     description:
       "A corduroy shirt is the perfect blend of cozy and polished. Pair with chinos or jeans for effortless styling",
     color: "Gray",
@@ -223,7 +223,7 @@ const productsList = [
   },
   {
     name: "Slim plaid Stretch Flannel Shirt",
-    imageURL: "/img/shirts/shirt4.jpg",
+    imageURL: "/img/shirts/mensShirt4.jpg",
     description:
       "An dressier, ultra-soft flannel infused with stretch and featured in a classic plaid for versatile style. Pair with dark denim for an everyday look",
     color: "Dark Brown",

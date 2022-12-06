9aeda260e5d290924fb07169cb32e99de85e9f60
JackPadalino <pada0867@gmail.com>
2022-12-05 21:48:04 -0500

    updated womensProducts

--- server/api/womensProducts.js
+++ server/api/womensProducts.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
 //const { Student, Campus } = require("../db");
+const sequelize = require("sequelize")
 
+// GETlocalhost:3000/api/women
+router.get("/", async (req, res, next) => {
+  });
+
+// GET localhost:3000/api/women/product
+// GET localhost:3000/api/women/product/productId
 module.exports = router;

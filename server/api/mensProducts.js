de90258d0c6f53318e6188eeec678aec8f47af5d
YiFan Chen <ychen289@buffalo.edu>
2022-12-05 22:28:46 -0500

    initial attempt at mensProduct GET routes

--- server/api/mensProducts.js
+++ server/api/mensProducts.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const router = express.Router();
-//const { Student, Campus } = require("../db");
-const sequelize = require("sequelize")
+const sequelize = require("sequelize");
+const { Product } = require("../db");
 
 // GETlocalhost:3000/api/mens
 router.get("/", async (req, res, next) => {
+  const menProducts = await Product.findAll({
+    where: {
+      gender: "men",
+    },
   });
+  res.send(menProducts);
+});
 
-// GET localhost:3000/api/mens/product
-// GET localhost:3000/api/mens/product/productId
+// GET localhost:3000/api/mens/:productType
+router.get("/:productType", async (req, res, next) => {
+  //sweater
+  const productType = req.params.productType;
+  const products = await Product.findAll({
+    where: {
+      gender: "men",
+      type: productType,
+    },
+  });
+  res.send(products);
+});
+
+// GET localhost:3000/api/mens/:productType/:productId
+router.get("/:productType/:productName", async (req, res, next) => {
+  //      sweater/1
+  const productName = req.params.productName;
+  const product = await Product.findByAll({
+    where: {
+      name: productName,
+    },
+  });
+  res.send(product);
+});
 module.exports = router;

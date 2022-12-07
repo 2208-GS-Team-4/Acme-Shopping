44c5d848f5f06e9a140b32ea4b69e7f76bd972e8
JackPadalino <pada0867@gmail.com>
2022-12-07 17:05:13 -0500

    removed commits related to user routes

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,5 @@ const router = express.Router();
 router.use('/auth', require('./auth'));
 router.use("/men", require("./mensProducts"));
 router.use("/women", require("./womensProducts"));
-router.use("/users", require("./users"));
 
 module.exports = router;

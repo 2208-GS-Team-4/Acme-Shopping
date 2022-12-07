85767d9763a8e5694ae57d9a896c0bc96980b701
JackPadalino <pada0867@gmail.com>
2022-12-07 12:43:35 -0500

    removed PK from CartProduct model

--- server/db/CartProduct.js
+++ server/db/CartProduct.js
@@ -2,12 +2,6 @@ const Sequelize = require("sequelize");
 const db = require("./db");
 
 const CartProduct = db.define('cartproduct', {
-    // id:{
-    //     type:Sequelize.INTEGER,
-    //     primaryKey:true,
-    //     autoIncrement:true,
-    //     allowNull:false
-    // },
     quantity: {
         type: Sequelize.INTEGER
         },

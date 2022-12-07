12a9081c732cb2714d1db5330297e7791762387e
JackPadalino <pada0867@gmail.com>
2022-12-07 12:44:04 -0500

    updated "Add to cart" route --> able to update quantity but have created a new bug related to price

--- server/api/users.js
+++ server/api/users.js
@@ -9,6 +9,22 @@ router.get("/", async (req, res, next) => {
     res.send(users);
   });
 
+router.get("/cartproducts", async (req, res, next) => {
+  const cartProducts = await CartProduct.findAll();
+  res.send(cartProducts);
+});
+
+router.get("/cartproducts/:id", async (req, res, next) => {
+  const cartProduct = await CartProduct.findByPk(req.params.id);
+  res.send(cartProduct);
+});
+
+// router.put("/cartproduct/:id", async (req, res, next) => {
+//   const cartProduct = await CartProduct.findByPk(req.params.id);
+//   await cartProduct.update({quantity:10});
+//   res.send(cartProduct);
+// });
+
 // GET localhost:3000/api/users/:userId
 router.get("/:userId", async (req, res, next) => {
   const user = await User.findByPk(req.params.userId);
@@ -34,25 +50,50 @@ router.get("/:userId/cart", async (req, res, next) => {
 // POST localhost:3000/api/users/:userId/cart
 
 // This route works but cannot add multiple of same item to cart!
-router.post("/:userId/cart", async (req, res, next) => {
+router.put("/:userId/cart", async (req, res, next) => {
+  let {
+    quantity,
+    price,
+    productId
+  } = req.body;
+  // find the cart associated with that user
   const cart = await Cart.findOne({
     where:{
       userId:req.params.userId
     }
   });
   const cartId = cart.id;
-  const {
-    quantity,
-    price,
-    productId
-  } = req.body;
-  await CartProduct.create({
-    quantity,
-    price,
-    cartId,
-    productId
+  // getting an error when trying to do this straight away --> having to update 
+  // the quantity instead of adding new item to table
+  // await CartProduct.create({
+  //   quantity,
+  //   price,
+  //   cartId,
+  //   productId
+  // });
+
+  // this code (lines 78-94) will work to update the quantity of the product in the user's car
+  // this creates a new bug --> what if the user adds the same item, but it now has a different price?
+  // we can't just update the price, because the price at the time of the first purchase was different!
+  const itemInCart = await CartProduct.findOne({
+    where:{
+      productId:productId
+    }
   });
+  if(!itemInCart){
+    await CartProduct.create({
+      quantity,
+      price,
+      cartId,
+      productId
+    });
+  }else{
+    const newQuantity = itemInCart.quantity += quantity;
+    await itemInCart.update({quantity:newQuantity});
+  };
   res.sendStatus(200);
 });
+
+
 
 module.exports = router;

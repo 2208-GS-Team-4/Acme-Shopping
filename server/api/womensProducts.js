91d3555d11843f3f95ba65f553518d00183d61ef
YiFan Chen <yifanchen@YiFans-MacBook-Air.local>
2022-12-08 22:03:39 -0500

    fixed refresh browser problem. changed backend routes to be simpler

--- server/api/womensProducts.js
+++ server/api/womensProducts.js
@@ -14,19 +14,19 @@ router.get("/", async (req, res, next) => {
 });
 
 // GET localhost:3000/api/women/:type
-router.get("/:type", async (req, res, next) => {
+router.get("/productType/:productType", async (req, res, next) => {
   const type = req.params.type;
   const products = await Product.findAll({
     where: {
       gender: "women",
-      type: type,
+      type: productType,
     },
   });
   res.send(products);
 });
 
 // GET localhost:3000/api/women/:type/:id
-router.get("/:productType/:id", async (req, res, next) => {
+router.get("/id/:id", async (req, res, next) => {
   const id = req.params.id;
   const product = await Product.findAll({ where: { id: id } });
   res.send(product);
--- src/components/SingleMenProduct.js
+++ src/components/SingleMenProduct.js
@@ -5,24 +5,17 @@ import { useParams } from "react-router-dom";
 const SingleMenProduct = () => {
   const [product, setProduct] = useState({});
   const { productType, id } = useParams();
-  const [loading, setLoading] = useState(true);
 
   const getProduct = async (productType, id) => {
     const response = await axios.get(`/api/men/${productType}/${id}`);
     setProduct(response.data);
-    setLoading(false);
   };
 
   useEffect(() => {
     getProduct(productType, id);
   }, []);
-  console.log(product[0]);
-  if (loading) {
-    return <p>Loading...</p>;
-  } else {
-    return <div>{product.name}</div>;
-    // return <div>hello</div>;
-  }
+
+  return <div> ({product} ? yes : no)</div>;
 };
 
 export default SingleMenProduct;

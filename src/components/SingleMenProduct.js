093c85c8126de1e8470099ae94d3142e4740b5f8
YiFan Chen <yifanchen@YiFans-MacBook-Air.local>
2022-12-12 01:50:48 -0500

    figured out singleMen product

--- src/components/SingleMenProduct.js
+++ src/components/SingleMenProduct.js
@@ -3,19 +3,43 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const SingleMenProduct = () => {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState([]);
   const { productType, id } = useParams();
 
-  const getProduct = async (productType, id) => {
-    const response = await axios.get(`/api/men/${productType}/${id}`);
+  const getProduct = async (id) => {
+    const response = await axios.get(`/api/men/id/${id}`);
+    console.log(response.data);
     setProduct(response.data);
   };
 
   useEffect(() => {
-    getProduct(productType, id);
+    getProduct(id);
   }, []);
 
-  return <div> ({product} ? yes : no)</div>;
+  return (
+    <div>
+      {product.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.imageURL} />
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>{item.color}</p>
+            <h3>${item.price}</h3>
+            {item.option.map((eachOption) => {
+              return (
+                <div key={eachOption.id}>
+                  <h4>Options Available:</h4>
+                  <p>Stock Available: {eachOption.stock}</p>
+                  <p>Size Available: {eachOption.size}</p>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default SingleMenProduct;

06733ed6106544f85727cb0981911b19b701bf4c
JackPadalino <pada0867@gmail.com>
2022-12-13 10:53:08 -0500

    added NavBar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,15 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { resetUser } from "../store/userSlice";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const { user } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  const logout = () => {
-    window.localStorage.removeItem("token");
-    dispatch(resetUser());
-  };
-
   return (
     <>
-      <div id="loggedInDiv">
-        {user.id ? (
-          <p>Welcome {user.username}!!</p>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-        {user.id && <button onClick={logout}>Logout</button>}
-      </div>
       <div className="adDiv">
         <img src="img/blouse/flowyBlouse2.png" />
         <img src="img/blouse/polkaDotBlouse2.png" />
         <img src="img/blouse/floralBlouse2.png" />
         <img src="img/blouse/fineKnitBlouse2.png" />
-      </div>{" "}
+      </div>
       <div className="adText">
         <h1>New Arrivals</h1>
         <p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { resetUser } from "../store/userSlice";
+
+const navStyle =  {
+    display: 'flex',
+    justifyContent:'space-evenly',
+    alignItems:'center'
+};
+
+const Navbar = () => {
+
+    const { user } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
+    const logout = () => {
+        window.localStorage.removeItem("token");
+        dispatch(resetUser());
+    };
+
+    return (
+        <div style={navStyle}>
+            <Link to="/">ACME</Link>
+            <Link to="/">Home</Link>
+            <Link to="/men">Men</Link>
+            <Link to="/women">Women</Link>
+            {user.id ? <p>Welcome {user.username}!!</p> : <Link to="/login">Login</Link>}
+            {user.id && <button onClick={logout}>Logout</button>}
+        </div>
+    );
+};
+
+export default Navbar;

1d2e3b3386427231593eb7edbb9c6b96cbbfef4d
JackPadalino <pada0867@gmail.com>
2022-12-12 22:51:52 -0500

    added component index

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+// single components
+export { default as AllMenProducts } from './AllmenProducts';
+export { default as AllWomenProducts } from './AllWomenProducts';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as ProductCategoryMen } from './ProductCategoryMen';
+export { default as ProductCategoryWomen } from './ProductCategoryWomen';
+export { default as Register } from './Register';
+export { default as RouterComponent } from './RouterComponent';
+export { default as SideMenuMen } from './SideMenuMen';
+export { default as SideMenuWomen } from './SideMenuWomen';
+export { default as SingleMenProduct } from './SingleMenProduct';
+export { default as SingleWomenProduct } from './SingleWomenProduct';

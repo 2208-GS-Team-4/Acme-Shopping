fee5f72fea272f705bfd67000d23958c263d538d
JackPadalino <pada0867@gmail.com>
2022-12-12 23:01:26 -0500

    updated components to use component index

--- src/components/AllMenProducts.js
+++ src/components/AllMenProducts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import SideMenuMen from "./";
+import {SideMenuMen} from "./";
 
 const AllMenProducts = () => {
   const [products, setProducts] = useState([]);

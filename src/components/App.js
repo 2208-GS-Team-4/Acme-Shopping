70e8c2b38118641ae9b60fc617c46dd3c113a35b
JackPadalino <pada0867@gmail.com>
2022-12-12 23:13:32 -0500

    updated comments on App component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import { setAllMensProducts,setAllWomensProducts } from "../store/productSlice";
 const App = () => {
   const dispatch = useDispatch();
 
+  // Here we make two API calls to get all Men's and Women's products when app first loads.
+  // We then update the Redux store with those arrays
   const fetchProducts = async() => {
     const allMensProducts = await axios.get('/api/men');
     dispatch(setAllMensProducts(allMensProducts.data));

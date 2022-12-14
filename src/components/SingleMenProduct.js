1260f969f864dc747bf2dce29cd5d71e4872162b
Heidi <lyu9287@gmail.com>
2022-12-14 18:57:55 -0500

    quantity

--- src/components/SingleMenProduct.js
+++ src/components/SingleMenProduct.js
@@ -53,6 +53,13 @@ const SingleMenProduct = () => {
                     );
                   })}
                 </select>
+                <select>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
               </div>
             </div>
           );

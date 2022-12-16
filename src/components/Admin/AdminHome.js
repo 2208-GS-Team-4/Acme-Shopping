071be6ffb221533c94d91bcfc6045be32cd706d4
JackPadalino <pada0867@gmail.com>
2022-12-16 08:28:35 -0500

    updated AdminHome component

--- src/components/Admin/AdminHome.js
+++ src/components/Admin/AdminHome.js
@@ -5,6 +5,8 @@ import { NotFoundPage } from '..';
 
 const AdminHome = () => {
     const { user } = useSelector((state) => state.user);
+    const { allProducts } = useSelector((state) => state.product);
+
     const [Type,setType] = useState('');
     const [Gender,setGender] = useState('');
     const [Name,setName] = useState('');
@@ -12,22 +14,50 @@ const AdminHome = () => {
     const [Price,setPrice] = useState('');
     const [Description,setDescription] = useState('');
 
-    // Need to add event listeners
-    // Need to filter the mens and womens products for an
-    // already exisiting product name
-    
+    const [validProductName,setValidProductName] = useState(false);
+    const [validProductErrorMessage,setValidProductErrorMessage] = useState(false);
+
+    const isValidProductName = productNameInput => {
+        const foundProduct = allProducts.find(product=>product.name===productNameInput);
+        if(foundProduct){
+            setValidProductName(false);
+            setValidProductErrorMessage(true);
+        }else{
+            setValidProductName(true);
+            setValidProductErrorMessage(false);
+        };
+    };
+
+    const handleTypeChange = (event) => {
+        setType(event.target.value);
+    };
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+    const handleColorChange = (event) => {
+        setColor(event.target.value);
+    };
+    const handlePriceChange = (event) => {
+        setPrice(event.target.value);
+    };
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
 
     if(!user.isAdmin) return <NotFoundPage />
     return (
         <>
             <h2>Add new product</h2>
             <form onSubmit={addNewProduct}>
-                <input required placeholder="Type" onChange={handleFirstNameChange}/>
-                <input required placeholder="Gender" onChange={handleLastNameChange}/>
-                <input required placeholder="Name" onChange={handleEmailChange}/>
-                <input required placeholder="Color" onChange={handleUsernameChange}/>
-                <input required placeholder="Price" onChange={handlePasswordChange}/>
-                <input required placeholder="Description" onChange={handlePasswordChange}/>
+                <input required placeholder="Type" onChange={handleTypeChange}/>
+                <input required placeholder="Gender" onChange={handleGenderChange}/>
+                <input required placeholder="Name" onChange={handleNameChange}/>
+                <input required placeholder="Color" onChange={handleColorChange}/>
+                <input required placeholder="Price" onChange={handlePriceChange}/>
+                <input required placeholder="Description" onChange={handleDescriptionChange}/>
                 <button>Add</button>
             </form>
         </>

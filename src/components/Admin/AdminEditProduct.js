dc9bae1e78309c4f00e991e3b681ddf4d57354ea
JackPadalino <pada0867@gmail.com>
2022-12-18 19:02:10 -0500

    updated component to render product information in form

--- src/components/Admin/AdminEditProduct.js
+++ src/components/Admin/AdminEditProduct.js
@@ -1,58 +1,116 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useParams,useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { Link,useParams,useNavigate } from "react-router-dom";
+import { NotFoundPage } from '..';
 
 const AdminEditProduct = () => {
-  const { allProducts } = useSelector((state) => state.product);
-  const { user } = useSelector((state) => state.user);
-  const [token,setToken] = useState(window.localStorage.getItem('token'));
-  const [product, setProduct] = useState([]);
-  const { id } = useParams();
+    // Redux variables
+    const { allProducts } = useSelector((state) => state.product);
+    const { user } = useSelector((state) => state.user);
 
-  const navigate = useNavigate();
+    // Page variables
+    const [token,setToken] = useState(window.localStorage.getItem('token'));
 
-  const getProduct = () => {
-    const foundProduct = allProducts.filter(
-      (product) => product.id === Number(id)
-    );
-    setProduct(foundProduct);
-  };
+    // Form variables
+    const [productUpdatedMessage,setProductUpdateddMessage] = useState(false);
 
-  const deleteProduct = async() =>{
-    try{
-        // will probably have to delete this product from the front end using Redux and store
-        await axios.delete(`/api/products/${id}`,{headers: {Authorization: 'Bearer ' + token}});
-        navigate("/admin");
-    }catch(error){
-        console.log(error);
+    // Product variables
+    const { id } = useParams();
+    const [product, setProduct] = useState([]);
+    const [name, setName] = useState('');
+    const [imageURL, setImageURL] = useState('');
+    const [description, setDescription] = useState('');
+    const [color, setColor] = useState('');
+    const [price, setPrice] = useState(0);
+    const [type, setType] = useState('');
+    const [gender, setGender] = useState('');
+
+    // Other variables
+    const navigate = useNavigate();
+
+    const getProduct = () => {
+        let foundProduct = allProducts.filter(product => product.id === Number(id));
+        foundProduct = foundProduct[0];
+        setProduct(foundProduct);
+        setImageURL(foundProduct.imageURL);
+        setName(foundProduct.name);
+        setDescription(foundProduct.description);
+        setColor(foundProduct.color);
+        setPrice(foundProduct.price);
+        setType(foundProduct.type);
+        setGender(foundProduct.gender);
     };
-};
 
-  useEffect(() => {
-    getProduct();
-  }, []);
+    const updateProduct = async (event) =>{
+        event.preventDefault();
+        console.log('product updated!')
+        // if(validProductName){
+        //     const body = {
+        //         name,
+        //         description,
+        //         color,
+        //         price,
+        //         type,
+        //         gender
+        //     };
+        //     await axios.put(`/api/products/${id}`, body,{headers: {Authorization: 'Bearer ' + token}});
+        //     setProductUpdatedMessage(true);
+        // };
+    };
 
-  return (
-    <>
-      <div>
-        {product.map((item) => {
-          return (
-            <div key={item.id}>
-              <img src={item.imageURL} className="singleProductImg" />
-              <div className="singleProductText">
-                <h2>{item.name}</h2>
-                <p>{item.description}</p>
-                <p>{item.color}</p>
-                <button onClick={()=>deleteProduct()}>delete</button>
-              </div>
+    const deleteProduct = async() =>{
+        try{
+            // will probably have to delete this product from the front end using Redux and store
+            await axios.delete(`/api/products/${id}`,{headers: {Authorization: 'Bearer ' + token}});
+            navigate("/admin");
+        }catch(error){
+            console.log(error);
+        };
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
+    const handleColorChange = (event) => {
+        setColor(event.target.value);
+    };
+    const handlePriceChange = (event) => {
+        setPrice(event.target.value);
+    };
+    const handleTypeChange = (event) => {
+        setType(event.target.value);
+    };
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    if(user.role!=='admin') return <NotFoundPage />
+    return (
+        <>
+            <img src={imageURL} className="singleProductImg" />
+            <div className="singleProductText">
+                <form onSubmit={updateProduct}>
+                    <input require value={name} onChange={handleNameChange}/>
+                    <input require value={description} onChange={handleDescriptionChange}/>
+                    <input require value={color} onChange={handleColorChange}/>
+                    <input require value={price} type='number' step='.01' onChange={handlePriceChange}/>
+                    <input require value={type} onChange={handleTypeChange}/>
+                    <input require value={gender} onChange={handleGenderChange}/>
+                    <button>Save</button>
+                </form>
+                {productUpdatedMessage && <p style={{color:'green',marginTop:'10px'}}>Product successfully updated.</p>}
+                <button onClick={()=>deleteProduct()}>Delete</button>
             </div>
-          );
-        })}
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default AdminEditProduct;

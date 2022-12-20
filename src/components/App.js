deb20a4113c8671cab4679699a6f3b1a66812370
JackPadalino <pada0867@gmail.com>
2022-12-19 21:52:07 -0500

    updated app.js to check for user token to keep user logged in

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { Navbar, RouterComponent } from "./";
+import { setUser } from '../store/userSlice';
 import {
   setAllProducts,
   setAllMensProducts,
@@ -22,8 +23,23 @@ const App = () => {
     dispatch(setAllWomensProducts(allWomensProducts.data));
   };
 
+  // Here we are checking to see if a user is already logged in upon refresh
+  // If a token is present in local storage, we keep the user logged in
+  const checkForUser = async () => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      const response = await axios.get("/api/auth", {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(setUser(response.data));
+    }
+  };
+
   useEffect(() => {
     fetchProducts();
+    checkForUser();
   }, []);
 
   return (

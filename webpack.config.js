3ec19fb311e9d4ae21016fc4beabc64c5b039755
Heidi <lyu9287@gmail.com>
2023-02-19 20:48:43 -0500

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,15 @@
 module.exports = {
-  mode: "development",
-  entry: ["./src/index.tsx"],
   devtool: "source-map",
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        include: /images/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "images/",
-              publicPath: "images/",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.ts$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
           presets: ["@babel/preset-react"],
         },
       },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: "ts-loader",
-      },
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".html", ".css"],
   },
 };
